21dfa060f98769683d89eceb729e63274f2cb4a7
David Oslander <doslande@redhat.com>
2017-06-18 13:01:53 -0400

    tried to modify marshalling process in ValidateGadgetRoute to no write the xml prolog in the output -- not successful.

--- src/main/java/org/doslande/OrderProcessor.java
+++ src/main/java/org/doslande/OrderProcessor.java
@@ -14,6 +14,13 @@ public class OrderProcessor implements Processor {
         exchange.setPattern(ExchangePattern.InOnly);
 
         System.out.println("Order block!");
+        
+        // alternative: get the Body as a string, 
+        // remove xml prolog from Body, then get Order
+        String tempBody = inMessage.getBody(String.class);
+        System.out.println("Order block-1! tempBody is: " + tempBody);
+        // now remove the xml prolog first line
+        
         Order order1 = inMessage.getBody(Order.class);            
         System.out.println("Order block! toString is: " + order1);
         exchange.getOut().setBody(order1);
--- src/main/java/org/doslande/ValidateGadgetRoute.java
+++ src/main/java/org/doslande/ValidateGadgetRoute.java
@@ -1,9 +1,14 @@
 package org.doslande;
 
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.Marshaller;
+
 import org.apache.camel.Endpoint;
+import org.apache.camel.Exchange;
 import org.apache.camel.builder.NoErrorHandlerBuilder;
 import org.apache.camel.builder.RouteBuilder;
 import org.doslande.model.MyValidationBean;
+import org.doslande.model.Order;
 import org.apache.camel.spi.DataFormat;
 import org.apache.camel.converter.jaxb.JaxbDataFormat;
 
@@ -25,31 +30,43 @@ public class ValidateGadgetRoute extends RouteBuilder {
 		
 		DataFormat jaxbformat = new JaxbDataFormat("org.doslande.model");
 		
-		from(CXF_RS_ENDPOINT_URI).process(new OrderProcessor())
-		.choice()
-			.when(method(MyValidationBean.class, "isKnownCustomer"))
-				.choice()
-					.when(method(MyValidationBean.class, "isOrderUnderLimit"))
-						// known customer and order is under the limit, send to fulfillment
-//						.to("xslt:orders.xsl")
-						.to("log:fulfillment1")
-						.marshal(jaxbformat)
-						.to(fulfillmentQueue)						
-//						.to("log:fulfillment2")
-					.otherwise()
-						// order is over the limit, send to accounting
-//						.to("xslt:orders.xsl")
-						.marshal(jaxbformat)
-						.to(accountingQueue)
-						.to("log:accounting-overlimit")
-				.endChoice()
-			.otherwise()
-				// not a known customer, send to accounting
-//				.to("xslt:orders.xsl")
-				.marshal(jaxbformat)
-	            .to(accountingQueue)
-				.to("log:accounting-newcustomer")
-		.endChoice();
+		JaxbDataFormat jaxbformat2 = new JaxbDataFormat("org.doslande.model");
+//		JAXBContext jc = jaxbformat2.getContext();
+		JAXBContext jc2 = JAXBContext.newInstance("org.doslande.model");
+		Marshaller marshaller = jc2.createMarshaller();
+		marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+		marshaller.setProperty("com.sun.xml.bind.xmlDeclaration", Boolean.FALSE);
+		marshaller.setProperty(Marshaller.JAXB_ENCODING, "UTF-8");
+		marshaller.setProperty(Marshaller.JAXB_FRAGMENT, Boolean.TRUE);
+		
+		from(CXF_RS_ENDPOINT_URI)
+		// maybe do xslt to remove the xml prolog
+		.process(new OrderProcessor())
+			.choice()
+				.when(method(MyValidationBean.class, "isKnownCustomer"))
+					.choice()
+						.when(method(MyValidationBean.class, "isOrderUnderLimit"))
+							// known customer and order is under the limit, send to fulfillment
+	//						.to("xslt:orders.xsl")
+							.to("log:fulfillment1")
+							.marshal(jaxbformat2)
+							.to("log:fulfillment2")
+							.to(fulfillmentQueue)						
+	//						.to("log:fulfillment2")
+						.otherwise()
+							// order is over the limit, send to accounting
+	//						.to("xslt:orders.xsl")
+							.marshal(jaxbformat)
+							.to(accountingQueue)
+							.to("log:accounting-overlimit")
+					.endChoice()
+				.otherwise()
+					// not a known customer, send to accounting
+	//				.to("xslt:orders.xsl")
+					.marshal(jaxbformat)
+		            .to(accountingQueue)
+					.to("log:accounting-newcustomer")
+			.endChoice();
 		
 		
 		
