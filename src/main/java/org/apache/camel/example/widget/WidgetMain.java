02183376eca00d8505aac9fa3f2dee8d69ff10c7
David Oslander <doslande@redhat.com>
2017-06-19 20:33:28 -0400

    fixed import

--- src/main/java/org/apache/camel/example/widget/WidgetMain.java
+++ src/main/java/org/apache/camel/example/widget/WidgetMain.java
@@ -18,7 +18,6 @@ package org.apache.camel.example.widget;
 
 import org.apache.activemq.camel.component.ActiveMQComponent;
 import org.apache.camel.main.Main;
-import org.doslande.CustomerRoute;
 import org.doslande.FtpToDivisionRoute;
 import org.doslande.RestToDivisionRoute;
 import org.doslande.ValidateGadgetRoute;
