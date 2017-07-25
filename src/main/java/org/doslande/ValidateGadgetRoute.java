a7337870a6a34989cefa442c629a42989233d2f8
David Oslander <doslande@redhat.com>
2017-07-25 13:45:48 -0400

    more cleanup in ValidateGadgetRoute

--- src/main/java/org/doslande/ValidateGadgetRoute.java
+++ src/main/java/org/doslande/ValidateGadgetRoute.java
@@ -26,77 +26,26 @@ public class ValidateGadgetRoute extends RouteBuilder {
 		
 		// now expects one "order" element in the body. not "orders". 
 		from(CXF_RS_ENDPOINT_URI)
-		.setExchangePattern(ExchangePattern.InOnly)
-		.process(new OrderProcessor())
-			.choice()
-				.when(method(MyValidationBean.class, "isKnownCustomer"))
+			.setExchangePattern(ExchangePattern.InOnly)
+				.process(new OrderProcessor())
 					.choice()
-						.when(method(MyValidationBean.class, "isOrderUnderLimit"))
-							// known customer & order is under limit; send to fulfillment
-							.to("sql:{{sql.insertOrder}}")
-							.to("log:fulfillment2")
+						.when(method(MyValidationBean.class, "isKnownCustomer"))
+							.choice()
+								.when(method(MyValidationBean.class, "isOrderUnderLimit"))
+									// known customer & order is under limit; send to fulfillment
+									.to("sql:{{sql.insertOrder}}")
+									.to("log:fulfillment2")
+								.otherwise()
+									// order is over the limit, send to accounting
+									.marshal(jaxbformat)
+									.to(accountingQueue)
+									.to("log:accounting-overlimit")
+							.endChoice()
 						.otherwise()
-							// order is over the limit, send to accounting
+							// not a known customer, send to accounting
 							.marshal(jaxbformat)
-							.to(accountingQueue)
-							.to("log:accounting-overlimit")
-					.endChoice()
-				.otherwise()
-					// not a known customer, send to accounting
-					.marshal(jaxbformat)
-		            .to(accountingQueue)
-					.to("log:accounting-newcustomer")
-			.endChoice();
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-/*		getContext().setTracing(true);
-		
-		from("cxfrs://http://localhost:9090/route?resourceClasses=org.doslande.NewOrdersResource")
-		.to("log:rest-input-log")
-		.log("Here is the message that was enriched: ${body}");*/
-		
-		
-//		from("rest:post:orders")
-//			.to("log:rest-xml-log");
-		
-		// REST DSL
-//		rest("/myRestApp")        
-//        	.post("/neworders").consumes("application/xml")
-//        	.to("log:rest-xml-log");
-		
-//		from(gadgetDivisionQueue)
-//		.split(xPathBuilder)
-//			.choice()
-//				.when(method(MyValidationBean.class, "isKnownCustomer"))
-//					.choice()
-//						.when(method(MyValidationBean.class, "isOrderUnderLimit"))
-//							// known customer and order is under the limit, send to fulfillment
-//							.to("xslt:orders.xsl")
-//							.to(fulfillmentQueue)
-//							.to("log:fulfillment")
-//						.otherwise()
-//							// order is over the limit, send to accounting
-//							.to("xslt:orders.xsl")
-//							.to(accountingQueue)
-//							.to("log:accounting-overlimit")
-//					.endChoice()
-//				.otherwise()
-//					// not a known customer, send to accounting
-//					.to("xslt:orders.xsl")
-//		            .to(accountingQueue)
-//					.to("log:accounting-newcustomer")
-//			.endChoice();
+				            .to(accountingQueue)
+							.to("log:accounting-newcustomer")
+					.endChoice();
 	}
-
 }
